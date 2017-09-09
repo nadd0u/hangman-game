81e42f23ddc1315bbf020f9e40cab45244c108bb
nadianm <nadia.nouimehidi@gmail.com>
2017-09-09 07:15:04 -0500

    Add files via upload

---
+++ Housewives Hangman/script.js
@@ -0,0 +1,154 @@
+window.onload = function () {
+
+  var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
+        'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
+        't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
+  var categories;         // Array of topics
+  var chosenCategory;     // Selected catagory
+  var getHint ;          // Word getHint
+  var word ;              // Selected word
+  var guess ;             // Geuss
+  var geusses = [ ];      // Stored geusses
+  var lives ;             // Lives
+  var counter ;           // Count correct geusses
+  var space;              // Number of spaces in word '-'
+
+  // when i was trying to display lives, for some reason mylives.innerHTMl
+  // didn't work? I dont really understand because catchphrase.innerhtml worked
+  // so I had to create this extra variable
+  var showLives = document.getElementById("mylives");
+
+
+  // make my buttons into a list of the alphabet
+  var buttons = function () {
+    myButtons = document.getElementById('buttons');
+    letters = document.createElement('ul');
+
+    for (var i = 0; i < alphabet.length; i++) {
+      letters.id = 'alphabet';
+      list = document.createElement('li');
+      list.id = 'letter';
+      list.innerHTML = alphabet[i];
+      check();
+      myButtons.appendChild(letters);
+      letters.appendChild(list);
+    }
+  }
+
+
+
+// depending on the word that is randomly selcted by the game, I want to display the corresponding catchphrase.
+// this is the part that hasn't worked for me, the word selected is random but the catchphrase doesnt correspond to it
+//I also tried to make each housewife an object so their catchphrase/words would be properties of the same object
+// but that didn't work either .
+  var selectCat = function () {
+    if (chosenCategory === categories[0]) {
+      catchphrase.innerHTML = "Im not getting older, im just getting _______";
+    } else if (chosenCategory === categories[1]) {
+      catchphrase.innerHTML = "God is my savior, my husband is my king, and my body? its ______";
+    } else if (chosenCategory === categories[2]) {
+      catchphrase.innerHTML = "Only god can judge me, and he seems quite ________";
+    }
+    else if (chosenCategory === categories[2]) {
+      catchphrase.innerHTML = "sometimes sojna has to go ________. what can I say?";
+    }
+  }
+
+// Create "______"
+   result = function () {
+    wordHolder = document.getElementById('hold');
+    correct = document.createElement('ul');
+
+    for (var i = 0; i < word.length; i++) {
+      correct.setAttribute('id', 'my-word');
+      guess = document.createElement('li');
+      guess.setAttribute('class', 'guess');
+      if (word[i] === "-") {
+        guess.innerHTML = "-";
+        space = 1;
+      } else {
+        guess.innerHTML = "_";
+      }
+
+      geusses.push(guess);
+      wordHolder.appendChild(correct);
+      correct.appendChild(guess);
+    }
+  }
+
+  // Show lives
+   comments = function () {
+    showLives.innerHTML = "You have " + lives + " lives";
+    if (lives < 1) {
+      showLives.innerHTML = "Game Over";
+    }
+    for (var i = 0; i < geusses.length; i++) {
+      if (counter + space === geusses.length) {
+        showLives.innerHTML = "You Win!";
+      }
+    }
+  }
+
+
+
+  // clicking a letter = a guess
+   check = function () {
+    list.onclick = function () {
+      var geuss = (this.innerHTML);
+      this.setAttribute("class", "active");
+      this.onclick = null;
+      for (var i = 0; i < word.length; i++) {
+        if (word[i] === geuss) {
+          geusses[i].innerHTML = geuss;
+          counter += 1;
+        }
+      }
+      var j = (word.indexOf(geuss));
+      if (j === -1) {
+        lives -= 1;
+        comments();
+        animate();
+      } else {
+        comments();
+      }
+    }
+  }
+
+
+  // here the game randomly selects a word from the list of categories
+  play = function () {
+    categories = [
+        ["blonder", "sinful", "impressed", "commando"]
+    ];
+
+    chosenCategory = categories[Math.floor(Math.random() * categories.length)];
+    word = chosenCategory[Math.floor(Math.random() * chosenCategory.length)];
+    word = word.replace(/\s/g, "-");
+    console.log(word);
+    buttons();
+
+    geusses = [ ];
+    lives = 10;
+    counter = 0;
+    space = 0;
+    result();
+    comments();
+    selectCat();
+  }
+
+  play();
+
+
+
+
+   // play again button
+
+  document.getElementById('reset').onclick = function() {
+    correct.parentNode.removeChild(correct);
+    letters.parentNode.removeChild(letters);
+    showClue.innerHTML = "";
+    context.clearRect(0, 0, 400, 400);
+    play();
+  }
+}
